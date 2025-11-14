299f13bb3bb5334dde6e1c9da6f7c07a199c95c3
polter-dev <144177408+polter-dev@users.noreply.github.com>
2025-11-14 18:12:08 -0500

    renamed main->client & created server to establish a connection during execution

---
+++ client.cpp
@@ -0,0 +1,17 @@
+//standard import
+#include <iostream>
+using namespace std;
+
+/*
+This program is fully developed and created by: Marcus Ruth 
+https://www.github.com/polter-dev/miniredis_cpp
+
+servers as the client that connects and communicates to the server
+*/
+
+
+
+int main(){
+    
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,17 @@
+//standard import
+#include <iostream>
+using namespace std;
+
+/*
+This program is fully developed and created by: Marcus Ruth 
+https://www.github.com/polter-dev/miniredis_cpp
+
+servers as the server that the client will conect to
+*/
+
+
+
+int main(){
+    
+    return 0;
+}
