120a44ab1309fdfb708d1383cc079051283ff29e
polter-dev <144177408+polter-dev@users.noreply.github.com>
2025-12-12 19:12:16 -0500

    created socket and binded to ip address (local)

--- server.cpp
+++ server.cpp
@@ -1,17 +1,48 @@
 //standard import
 #include <iostream>
+#include <stdlib.h>
+#include <string.h>
 using namespace std;
+
+//import for to grab socket / ip includes necessaryq
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
 /*
 This program is fully developed and created by: Marcus Ruth 
 https://www.github.com/polter-dev/miniredis_cpp
 
-servers as the server that the client will conect to
+Program Synopsis: client that connects and communicates to the server to simulate a redis like system
 */
+int grabSocket(){
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1){
+        cout << "Could not grab server socket" << endl;
+        exit(errno);
+    }
+    return fd;
+}
 
+void initServerSocket(struct sockaddr_in *s1){
+    s1->sin_family = AF_INET;
+    s1->sin_port = htons(1234);
+    s1->sin_addr.s_addr = htonl(INADDR_ANY);
+}
 
+void bindSock(int fd, struct sockaddr_in *serverSocket){
+    int res = ::bind(fd, (struct sockaddr *)serverSocket, sizeof(*serverSocket));
+    if (res == -1){
+        cout << "Could not bind to socket!\n" << endl;
+        exit(-1);
+    }
+}
 
 int main(){
-    
+    int fd = grabSocket();
+    struct sockaddr_in serverSocket; initServerSocket(&serverSocket);
+
+
     return 0;
 }
